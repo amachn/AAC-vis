3553e1eb0e002f27aa1dee45d9c3e5c181e2a6e1
amachn <amachann2005@gmail.com>
2024-11-21 23:17:00 -0600

    fix: match ratelimit + add docs

--- geocode.py
+++ geocode.py
@@ -2,6 +2,7 @@ from csv import QUOTE_NONNUMERIC
 from dotenv import load_dotenv
 from os import getenv
 from requests import get
+from time import sleep
 import pandas as pd
 import re
 
@@ -21,21 +22,22 @@ class Geocoder:
     def _geocode(self, address: str) -> tuple[int, int] | None:
         req = get(f"https://geocode.maps.co/search?q={address}&api_key={self.key}")
 
-        if len(req) != 0:
+        if len(req) != 0: # successful query for coordinates
             data = req.json()[0]
             return (data['lat'], data['lon'])
-        else:
+        else: # if len is 0, then the request was bad and yielded no coordinates
             return (-1, -1)
 
     def run(self, count: int) -> None:
-        start_idx = int(self.out_df.tail(1)["idx"].values[0]) + 1
-        new_rows = []
+        start_idx = int(self.out_df.tail(1)["idx"].values[0]) + 1 
+        new_rows = [] 
 
         for idx in range(start_idx, start_idx + count):
-            row = self.raw_df.query("idx == @idx")
+            row = self.raw_df.query("idx == @idx") # find row with matching idx
             base_addr = row["address"].values[0]
 
             if base_addr != "Outside Jurisdiction" and re.match("\\d", base_addr):
+                # regexes to format addr for GET request
                 addr = re.sub("[()]", "", base_addr)
                 addr = re.sub("\\Win\\W", " ", addr)
                 addr = re.sub("\\W", "+", addr)
@@ -48,12 +50,19 @@ class Geocoder:
 
             new_rows.append(vals)
 
+            sleep(1.2) # avoid hitting API ratelimit of 1req/s
+
         df_extension = pd.DataFrame(new_rows, columns=self.out_df.columns)
         df_extension.to_csv("dat/geocoded_addrs.csv", mode='a',
                             index=False, header=False,
                             quoting=QUOTE_NONNUMERIC)
 
-       
+
+# TO RUN THIS SCRIPT:
+# 1. must have a valid API key from geocode.maps.co
+#   - API key must also be placed in a .env file located at ./
+# 2. buildDataset.R must be run to generate .csv files       
+
 if __name__ == "__main__":
     geocoder = Geocoder()
 
